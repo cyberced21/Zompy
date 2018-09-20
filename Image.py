1307ee15c9c0a33b0f8ecc0906f068c5b35b8dc4
Sebas1992 <msebastien1696@gmail.com>
2018-09-20 17:40:30 -0400

    Ajout de la classe Image

---
+++ Image.py
@@ -0,0 +1,14 @@
+"""
+    Classe representant une image dans le jeu Zompy
+"""
+
+class Image:
+    """
+        Attributs:
+            String: path
+        Methodes:
+            getPath: Retourne le chemin de l'image
+    """
+
+    def __init__(self, String path)
+        self.path = path
