81181d1944f6e4720c2c558a71438d048fae6498
Sebas1992 <msebastien1696@gmail.com>
2018-10-19 17:31:53 -0400

    Ajout du score

--- zompy.py
+++ zompy.py
@@ -7,6 +7,7 @@ pygame.init()
 pygame.mixer.init() #Si on veux ajouter du son
 fenetre = pygame.display.set_mode((constantes.LARGEUR, constantes.HAUTEUR))
 clock = pygame.time.Clock()
+score = 0
 
 ennemis = pygame.sprite.Group()
 spriteGroup = pygame.sprite.Group()
@@ -17,6 +18,15 @@ for i in range(8):
     spriteGroup.add(e)
     ennemis.add(e)
 spriteGroup.add(joueur)
+
+# Fonction a integrer a la classe du Jeu
+def drawScore(surf, score):
+    font_name = pygame.font.match_font('arial')
+    font = pygame.font.Font(font_name, 24)
+    text_surface = font.render("Score: " + score, True, (255, 0, 0))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (constantes.LARGEUR/2, 0)
+    surf.blit(text_surface, text_rect)
 
 # Game loop
 running = True
@@ -36,10 +46,12 @@ while running:
 
     # Update
     spriteGroup.update()
+
     # Verifie si une balle a touche un ennemi
     hits = pygame.sprite.groupcollide(ennemis, balles, True, True)
     # Si oui, on le regenere
     for hit in hits:
+        score += 1
         e = personnages.Ennemi()
         spriteGroup.add(e)
         ennemis.add(e)
@@ -52,6 +64,7 @@ while running:
     # Dessiner
     fenetre.fill((56, 26, 164))
     spriteGroup.draw(fenetre)
+    drawScore(fenetre, str(score))
 
     # A faire apres avoir tout dessiner
     pygame.display.flip()
