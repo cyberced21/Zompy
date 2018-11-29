abf8b4f4cb96ef074bac848446ff9c761b2b2a2c
Samuel <sam.vaudreuil@hotmail.com.com>
2018-11-29 10:30:07 -0500

    Suppression de code inutile dans zompy

--- zompy.py
+++ zompy.py
@@ -14,7 +14,6 @@ imagePerso = ""
 menu = menu.Menu()
 selectMenu = selectionPersonnage.SelectionPersonnage()
 action = ""
-action2 = ""
 clock = pygame.time.Clock()
 while True:
     clock.tick(constantes.FPS)
@@ -26,21 +25,19 @@ while True:
     elif(action == "score"):
         menu.run()
     elif(action == "jouer"):
-        #clock.tick(constantes.FPS)
-        selectMenu.run()
-        action2 = selectMenu.run()
+        action = selectMenu.run()
 
-        if(action2 == "perso1"):
+        if(action == "perso1"):
             imagePerso = constantes.perso1
 
-        elif(action2 == "perso2"):
+        elif(action == "perso2"):
             imagePerso = constantes.perso2
 
-        elif(action2 == "perso3"):
+        elif(action == "perso3"):
             imagePerso = constantes.perso3
 
         partie = Partie()
         partie.jouer()
-        #menu.run()
 
 pygame.quit()
+quit()
